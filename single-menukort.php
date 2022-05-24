7b729d5086fa7060fe7c14e69d726e1c58c8c8a5
Charlotte Franciska Christiansen <char308@stud.kea.dk>
2022-05-24 11:57:28 +0200

    link til array

--- single-menukort.php
+++ single-menukort.php
@@ -26,7 +26,7 @@ while ( have_posts() ) :
 </main>
 <script>
 	let retter = []
-	const url = "https://http://charlottefranciska.dk/kea/rosetta/wp-json/wp/v2/retter";
+	const url = "http://charlottefranciska.dk/kea/rosetta/wp-json/wp/v2/retter";
 async function getJson (){
 	let response = await fetch(url);
 	retter = await response.json (); 
