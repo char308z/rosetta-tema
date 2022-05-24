935ffdb379ae27cad436901834642c7fd9dad46c
Charlotte Franciska Christiansen <char308@stud.kea.dk>
2022-05-24 12:22:01 +0200

    ...

--- single-menukort.php
+++ single-menukort.php
@@ -9,10 +9,6 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit; // Exit if accessed directly.
 }
 ?>
-<?php
-while ( have_posts() ) :
-	the_post();
-	?>
 
 <main id="content" <?php post_class( 'site-main' ); ?> role="main">
 	<div class="page-content">
