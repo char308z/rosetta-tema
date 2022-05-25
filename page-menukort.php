549d3147ca01f30a9fcd28559370a4b6b472d578
Charlotte Franciska Christiansen <char308@stud.kea.dk>
2022-05-25 09:12:43 +0200

    page-menukort.php

---
+++ page-menukort.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * The site's entry point.
+ *
+ * Loads the relevant template part,
+ * the loop is executed (when needed) by the relevant template part.
+ *
+ * @package HelloElementor
+ */
+
+
+get_header();
+?>
+<body>
+	hello
+</body>
+
+<?php
+get_footer();
+?>
