4a29add61b34529119114b084c0dfa1e90f04239
Charlotte Franciska Christiansen <char308@stud.kea.dk>
2022-06-01 10:45:08 +0200

    page array singleview

--- page-delikatesser.php
+++ page-delikatesser.php
@@ -14,6 +14,17 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 get_header();
 ?>
+<aside id="popup">
+      <button id="lukKnap">X</button>
+
+      <article>
+        <img src="" alt="" />
+        <h3 id="popupTitle" class="navn"></h3>
+        <p class="langbeskrivelse"></p>
+        <h4 class="pris"></h4>
+		<button>Tilføj</button>
+      </article>
+    </aside>
 
 <main>
 	<h1>Delikatesser</h1>
@@ -36,11 +47,13 @@ get_header();
 	<article>
 		<img src="" alt="">
 		<h3 id="deliTitle" class="navn">Navn</h3>
-		<p class="beskrivelse"></p>
-		<p class="pris"></p>
-		<button>Bestil</button>
+		<p id="deliBeskrivelse"class="beskrivelse"></p>
+		<h4 class="pris"></h4>
+		<button>Tilføj</button>
 	</article>
 </template>
+
+   
 
 	<script>
 	let delikatesser = [];
@@ -49,6 +62,7 @@ get_header();
 	const loop = document.querySelector("#loopView");
     const template = document.querySelector("template").content;
 	const knapListe = document.querySelector("#knapper");
+	const popup = document.querySelector("#popup");
 	const deliUrl = ("http://charlottefranciska.dk/kea/rosetta/wp-json/wp/v2/delikatesse?per_page=100");
 	const kateUrl = ("http://charlottefranciska.dk/kea/rosetta/wp-json/wp/v2/kategorier");
 
@@ -104,14 +118,23 @@ function visDelikatesser (){
             klon.querySelector(".beskrivelse").textContent =
               delikatesse.beskrivelse;
             klon.querySelector(".pris").textContent =
-              "Pris: " + delikatesse.pris ;
+              delikatesse.pris + " kr." ;
             klon.querySelector("img").src = delikatesse.billede.guid;
-			  klon.querySelector("article").addEventListener("click",()=>{location.href = delikatesse.link});
+			  klon.querySelector("article").addEventListener("click",()=> visDetaljer(delikatesse));
 			loop.appendChild(klon);
-          
           }
         });
 
+		function visDetaljer (delikatesse){
+			console.log(this); 
+popup.querySelector("h3").textContent = delikatesse.title.rendered;
+            popup.querySelector(".langbeskrivelse").textContent =
+              delikatesse.lang_beskrivelse;
+            popup.querySelector(".pris").textContent = delikatesse.pris + " kr.";
+            popup.querySelector("img").src = delikatesse.billede.guid;
+			popup.style.display="block";
+		}
+document.querySelector("#lukKnap").addEventListener("click",() => (popup.style.display = "none"));
 }
 
 </script>
